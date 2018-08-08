ecfdeb4049a107641cc9f3d12fe2ea2fc2c49752
Albert Wang <amwang217@gmail.com>
2018-08-07 20:58:58 -0700

    Move locale file to its own directory. Remove unused hello_react jsx

--- app/javascript/packs/brave_rewards_banner_display.jsx
+++ app/javascript/packs/brave_rewards_banner_display.jsx
@@ -6,18 +6,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import SiteBanner from 'brave-ui/features/rewards/siteBanner'
+import { initLocale } from 'brave-ui'
+import locale from 'locale/en'
 
 class BraveRewardsPageForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {title: 'YOUR TITLE'};
-    console.log("New title:" + this.state.title);
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {title: 'YOUR TITLE', description: 'A brief description'};
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+  handleTitleChange(event) {
     this.setState({title: event.target.value});
+  }
+
+  handleDescriptionChange(event) {
+    this.setState({description: event.target.value});
   }
 
   handleSubmit(event) {
@@ -25,14 +31,12 @@ class BraveRewardsPageForm extends React.Component {
     event.preventDefault();
   }
 
-  getTitle () {
-    return this.state.title;
-  }
-
   render() {
     const shrunkHeight = {
-      height: '567px'
+      height: '50vh'
     };
+
+    initLocale(locale);
 
     return (
       <div>
@@ -59,15 +63,22 @@ class BraveRewardsPageForm extends React.Component {
                 "selected": false
               }
             ]}
-          >Hello World<br/>I wonder if this works</SiteBanner>
+          >{this.state.description}</SiteBanner>
         </div>
         <div id="controller_form">
+          <hr/>
           <h4>PREVIEW</h4>
           <form onSubmit={this.handleSubmit}>
             <label>
               Title:
-              <input type="title" value={this.state.value} onChange={this.handleChange} />
+              <input type="title" value={this.state.value} onChange={this.handleTitleChange} />
             </label>
+            <div>
+              Description:
+              <label>
+                <textarea type="description" value={this.state.description} onChange={this.handleDescriptionChange} />
+              </label>
+            </div>
             <input type="submit" value="Submit"/>
           </form>
         </div>
@@ -78,42 +89,9 @@ class BraveRewardsPageForm extends React.Component {
 
 const braveRewardsPageForm = <BraveRewardsPageForm />;
 
-var newDiv = document.createElement('div');
-
-/*
-ReactDOM.render(
-  <SiteBanner 
-    bgImage={"https://www.popsci.com/sites/popsci.com/files/styles/655_1x_/public/images/2017/07/mars-surface.jpg?itok=wZc9vU-e&fc=50,50"}
-    logo={"https://pbs.twimg.com/profile_images/920841899992236032/mSDhBoC9_400x400.jpg"}
-    title={"Alexis Ren"}
-    currentDonation={"5"}
-    donationAmounts={[
-      {
-        "tokens": 1,
-        "converted": 0.3,
-        "selected": false
-      },
-      {
-        "tokens": 5,
-        "converted": 1.5,
-        "selected": false
-      },
-      {
-        "tokens": 10,
-        "converted": 3,
-        "selected": false
-      }
-    ]}
-  >Hello World<br/>I wonder if this works</SiteBanner>,
-  document.body.appendChild(newDiv),
-)
-*/
-
-var newContent = document.createElement("div")
-newDiv.appendChild(document.createElement("div"));
-
 ReactDOM.render(
   braveRewardsPageForm,
-  document.body.appendChild(newDiv)
-//  newDiv.children[0].appendChild(document.createElement("div"))
+  document.body.appendChild(document.createElement("div"))
 )
+
+document.getElementById('site_banner').children[0].style.height = '50vh';
